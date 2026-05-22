faf9d85d2d65fd136579ff76f0ec4db54d83a205
Mohammad Emad Hassan <mohammed352hasan@gmail.com>
2026-05-23 01:04:41 +0300

    Clear token and admin IDs in teletoken.py
    Removed hardcoded token and admin IDs.

--- teletoken.py
+++ teletoken.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
 
-Token="8635793760:AAEqXUpDbx_DZmBao2GIZbpFUKDtiw3CA04"
+Token=""
 DB_PATH = os.path.join(os.path.dirname(__file__), "testbot.db")
-ADMIN_IDS = [ 6514704691,753253599,1011884696]
+ADMIN_IDS = [ ]
